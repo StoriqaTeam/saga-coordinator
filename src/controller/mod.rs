0781995808cef9c80efb37b0600e932199c5ec93
Sergey Efremov <5125402@mail.ru>
2018-04-04 14:41:04 +0300

    added logger format

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,14 +1,49 @@
 pub mod routes;
 
-use config::Config;
-use self::routes::Route;
 
 use std::sync::Arc;
-use stq_http::client::ClientHandle;
 use stq_router::RouteParser;
 
-pub struct Controller {
-    pub route_parser: Arc<RouteParser<Route>>,
+use stq_http::client::ClientHandle as HttpClientHandle;
+use stq_http::controller::{Controller};
+use stq_http::errors::ControllerError;
+use stq_http::request_util::{read_body, ControllerFuture};
+use stq_http::request_util::serialize_future;
+
+use futures::prelude::*;
+use futures::future;
+use hyper::{Method};
+use hyper::server::{ Request};
+
+use config::Config;
+use self::routes::Route;
+use ops;
+
+
+pub struct ControllerImpl {
     pub config: Config,
-    pub client_handle: ClientHandle,
+    pub http_client: Arc<HttpClientHandle>,
+    pub route_parser: Arc<RouteParser<Route>>,
+}
+
+impl Controller for ControllerImpl {
+    fn call(&self, req: Request) -> ControllerFuture {
+        match (req.method(), self.route_parser.test(req.path())) {
+
+            (&Method::Post, Some(Route::CreateAccount)) => serialize_future(
+                read_body(req.body())
+                    .map_err(|e| ControllerError::UnprocessableEntity(e.into()))
+                    .and_then({
+                        let http_client = self.http_client.clone();
+                        let config = self.config.clone();
+                        println!("Create account");
+                        move |s| {
+                            ops::account::create(http_client.clone(), config.clone(), s)
+                                .map_err(|e| ControllerError::InternalServerError(e))
+                        }
+                    }),
+            ),
+            _ => Box::new(future::err(ControllerError::NotFound)),
+        }
+    }
 }
--- src/controller/routes.rs
+++ src/controller/routes.rs
@@ -5,7 +5,7 @@ pub enum Route {
     CreateAccount,
 }
 
-pub fn create_router() -> RouteParser<Route> {
+pub fn create_route_parser() -> RouteParser<Route> {
     let mut router = RouteParser::default();
 
     router.add_route(r"^/create_account", || Route::CreateAccount);
