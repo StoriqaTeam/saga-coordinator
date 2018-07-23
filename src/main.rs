d020bfa25f80f47145e04ffa50cb62723583479e
Serejka <5125402@mail.ru>
2018-07-23 10:48:24 +0300

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate saga_coordinator_lib as lib;
+extern crate stq_logging;
 
 fn main() {
     let config = lib::config::Config::new().expect("Failed to load service configuration. Please check your 'config' folder");
+
+    // Prepare logger
+    stq_logging::init(config.graylog.as_ref());
+
     lib::start_server(config);
 }
