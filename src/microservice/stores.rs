bb61e960d9b6ed692bf666815342508c45116118
busarovalex <busarovalex@yandex.ru>
2018-12-19 11:14:29 +0300

    Use model urls instead if string literals

--- src/microservice/stores.rs
+++ src/microservice/stores.rs
@@ -41,7 +41,7 @@ pub struct StoresMicroserviceImpl<T: 'static + HttpClient + Clone> {
 
 impl<T: 'static + HttpClient + Clone> StoresMicroservice for StoresMicroserviceImpl<T> {
     fn deactivate_product(&self, initiator: Option<Initiator>, product_id: ProductId) -> ApiFuture<Product> {
-        let url = format!("{}/products/{}", self.stores_url(), product_id);
+        let url = format!("{}/{}/{}", self.stores_url(), StqModel::Product.to_url(), product_id);
         Box::new(
             super::request::<_, (), _>(self.http_client.clone(), Method::Delete, url, None, initiator.map(Into::into)).map_err(|e| {
                 e.context("Deactivate product in stores microservice failed.")
@@ -52,7 +52,7 @@ impl<T: 'static + HttpClient + Clone> StoresMicroservice for StoresMicroserviceI
     }
 
     fn deactivate_store(&self, initiator: Option<Initiator>, store_id: StoreId) -> ApiFuture<Store> {
-        let url = format!("{}/stores/{}", self.stores_url(), store_id);
+        let url = format!("{}/{}/{}", self.stores_url(), StqModel::Store.to_url(), store_id);
         Box::new(
             super::request::<_, (), _>(self.http_client.clone(), Method::Delete, url, None, initiator.map(Into::into)).map_err(|e| {
                 e.context("Deactivate store in stores microservice failed.")
@@ -63,7 +63,7 @@ impl<T: 'static + HttpClient + Clone> StoresMicroservice for StoresMicroserviceI
     }
 
     fn deactivate_base_product(&self, initiator: Option<Initiator>, base_product_id: BaseProductId) -> ApiFuture<BaseProduct> {
-        let url = format!("{}/base_products/{}", self.stores_url(), base_product_id);
+        let url = format!("{}/{}/{}", self.stores_url(), StqModel::BaseProduct.to_url(), base_product_id);
         Box::new(
             super::request::<_, (), _>(self.http_client.clone(), Method::Delete, url, None, initiator.map(Into::into)).map_err(|e| {
                 e.context("Deactivate base product in stores microservice failed.")
